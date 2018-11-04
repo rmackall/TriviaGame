c35c05b8e121d5664c2875830480d2199dfddfa1
Rich Mackall <richmackall@Richs-MacBook-Air.local>
2018-11-04 10:35:19 -0700

    Trivia Homework

---
+++ Assets/JS/app.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+    questions: "Jayson Peterson went to which DPS School?",
+    answers: {
+        a: "East",
+        b: "Thomas Jefferson",
+        c: "South High"
+    },
+
+    correctAnswer: "a"
+},
+
+{
+    questions: "Chauncey Billups went to which DPS School?",
+    answers: {
+        a: "Manual",
+        b: "Montbello",
+        c: "George Washington"
+    },
+
+    correctAnswer: "c"
+},
+
+{
+    questions: "Phillip Lindsey went to which DPS School?",
+    answers: {
+        a: "South High",
+        b: "West High",
+        c: "Abraham Lincoln"
+    },
+
+    correctAnswer: "a"
+},
+
+{
+    questions: "Kyle Freeland went to which DPS School?",
+    answers: {
+        a: "East",
+        b: "Thomas Jefferson",
+        c: "Manual"
+    },
+
+    correctAnswer: "b"
+}];
+
+var quizContainer = document.getElementById("quiz");
+
+var resultsContainer = document.getElementById("results");
+
+var submitButton = document.getElementById("sumbit");
+
+
